db83508a9ae166b3bac7c68af0b5a460d76d1719
Ahmad Fakhri Mirfananda <fakhri1304@gmail.com>
2021-11-24 13:46:35 +0700

    added more API

--- services/api/controller/subjectController.js
+++ services/api/controller/subjectController.js
@@ -25,12 +25,38 @@ const getAllSubject = async (req,res) => {
       }
 }
 
-const getSubjectById = async (req,res) => {
-  const id = parseInt(req.params.id);
-  const query = 'SELECT * FROM subject WHERE subject_id = $1;';
+const getSubjectByUserId = async (req,res) => {
+  const user_id = parseInt(req.params._user_id);
+  const query = 'SELECT * FROM subject WHERE user_id = $1 ORDER by subject_id;';
 
   try{
-      const { rows } = await dbQueries(query, [id]);
+      const { rows } = await dbQueries(query, [user_id]);
+      const dbResponse = rows;
+      if (dbResponse[0] === undefined) {
+        errorMessage.error = 'There are no subject';
+        return res
+          .status(status.error)
+          .send(errorMessage.error + ' ' + error.code);
+      }
+      successMessage.data = dbResponse;
+      return res  
+        .status(status.success)
+        .send(successMessage.data);
+    } catch (error) {
+      errorMessage.error = 'An error occured.'
+      return res
+        .status(status.error)
+        .send(errorMessage.error + ' ' + error.code);
+    }
+}
+
+const getSubjectById = async (req,res) => {
+  const id = parseInt(req.params.id);
+  const user_id = parseInt(req.params._user_id);
+  const query = 'SELECT * FROM subject WHERE subject_id = $1 AND user_id = $2;';
+
+  try{
+      const { rows } = await dbQueries(query, [id, user_id]);
       const dbResponse = rows;
       if (dbResponse[0] === undefined) {
         errorMessage.error = 'There are no subject';
@@ -54,7 +80,7 @@ const addSubject = async (req, res) => {
   /*const brand = (req.body.brand);
   const country = (req.body.country);
   const description = (req.body.description);*/
-  const query = 'INSERT INTO subject(name, user_id) VALUES(\'Rekayasa Perangkat Lunak\', 1);';
+  const query = 'INSERT INTO subject(name, user_id) VALUES(\'Rekayasa Perangkat Lunak\', 2);';
 
     try {
         const { rows } = await dbQueries(query);
@@ -80,5 +106,7 @@ const addSubject = async (req, res) => {
 module.exports = {
     getAllSubject,
     getSubjectById,
-    addSubject
+    addSubject,
+    getSubjectByUserId
+    
 }
--- services/api/routes/subjectRoutes.js
+++ services/api/routes/subjectRoutes.js
@@ -3,7 +3,8 @@ const controller = require('../controller/subjectController')
 
 // router for appetizer
 router.get('/', controller.getAllSubject);
-router.get('/:id', controller.getSubjectById);
+router.get('/:user_id/', controller.getSubjectByUserId);
+router.get('/:user_id/:id', controller.getSubjectById);
 router.post('/add/', controller.addSubject);
 
 module.exports = router;
